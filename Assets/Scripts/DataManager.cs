6f2e5b53395e4a98536dd62453fa83f26a16cc0c
DanielJMus <danielmus1999@hotmail.com>
2019-11-22 15:52:43 +1300

    Implement random data generation.

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager
+{
+
+    private float[,,] data;
+
+    public DataManager (int x, int y, int z)
+    {
+        data = new float[x,y,z];
+    }
+
+    // Generate randomized terrain data
+    public void Generate () {
+        for(int x = 0; x < data.GetLength(0); x++) {
+            for(int y = 0; y < data.GetLength(1); y++) {
+                for(int z = 0; z < data.GetLength(2); z++) {
+                    data[x,y,z] = Utils.Perlin3D((float)x / 8, (float)y / 8, (float)z / 8);
+                }
+            }
+        }
+    }
+
+    public float[,,] GetData()
+    {
+        return data;
+    }
+}
---
+++ Assets/Scripts/Terrain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+
+    [SerializeField, Range(0,1)] private float surfaceLevel = 0.5f;
+    private DataManager dataManager;
+    [SerializeField] Vector3Int WorldSize;
+
+    void Start()
+    {
+        dataManager = new DataManager(WorldSize.x, WorldSize.y, WorldSize.z);
+        dataManager.Generate();
+    }
+
+    void OnDrawGizmosSelected () {
+        for(int x = 0; x < WorldSize.x; x++)
+            for(int y = 0; y < WorldSize.y; y++)
+                for(int z = 0; z < WorldSize.z; z++) {
+                    if(dataManager.GetData()[x, y, z] < surfaceLevel) {
+                        Gizmos.DrawCube(new Vector3(x, y, z), Vector3.one / 4);
+                    }
+                }
+    }
+}
